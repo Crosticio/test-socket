de9193a81290d9148fb84877269115592e7a7765
Dylan Steven Ariza Rueda <Dyes.0920@gmail.com>
2021-11-06 20:27:53 -0500

    A little some of design not bad

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,15 +2,21 @@ const socket = io()
 
 const inputMessage = document.getElementById('message')
 const chat = document.querySelector('.chat')
+const sendButton = document.getElementById('sendMessage')
 
-inputMessage.addEventListener('keypress', e => {
-    if (e.key == 'Enter') {
-        socket.emit('chat:message', {
-            message: inputMessage.value
-        })
-    }
+sendButton.addEventListener('click', e => {
+    socket.emit('chat:message', {
+        message: inputMessage.value
+    })
+    inputMessage.value = ''
 })
 
 socket.on('chat:message', message => {
-    chat.innerHTML += `<p> ${message} </p>`
+    chat.innerHTML += `
+                <div class="content-text">
+                    <div class="text-message">
+                        <p class="text-message"> ${message} </p>
+                    </div>
+                </div>
+    `
 })
